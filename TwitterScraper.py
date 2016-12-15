63b5f9afc3c5617f3a2155acbda027d7d46073d0
Spencer Dean Stolworthy <sdstolworthy@gmail.com>
2016-12-14 19:42:46 -0700

    added comments to twitterscraper

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -3,12 +3,15 @@ import requests
 import oauth2
 import json
 class TwitterScraper:
+    #Global variables contain data needed to make twitter request
+    #timeline_endpoints is a listing of twitter timelines to pull in
     clientKey = "dBZVGtMSRyuHphDRadmo2Oytb"
     clientSecret = "ZJ1vxBtVeM9Sq4WML7uyfuJxyHArRhivo8MUfeVfwPCXkGTYl9"
     timeline_endpoints = ["https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TheFlightDeal",
             "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=FareDealAlert",
             "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=airfarewatchdog"]
     def oauth_req(self, url,secret = None, http_method="GET", post_body=b"",http_headers=None):
+        #this function returns the result of a call to the twitter api
         consumer = oauth2.Consumer(key=self.clientKey,secret=self.clientSecret)
         token = oauth2.Token(key="3415862773-zCUjEYxs9sS0XYuxGfrgmYjuV6m0qooSK1AmNB4", secret="UCSPCeEcoGk0aYGJL08YS2TqtdgUQCVcHUIOngW07U0kp")
         client = oauth2.Client(consumer, token)
@@ -17,8 +20,10 @@ class TwitterScraper:
     def getTwitterDeals(self):
         tweetDeals = []
         for url in self.timeline_endpoints:
+            #Make request to twitter API
             tweets = self.oauth_req(url)
             tweets = json.loads(tweets)
+            #the loop cleans out irrelevant data. This is specially customized for each twitter feed
             for tweet in tweets:
                 if "airfarewatchdog" in url:
                     if "#airfare" in tweet['text']:
